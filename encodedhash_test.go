0694a0aac504053c04ebf0e9d3fe4f329f551071
Jesse Steig <jsteig@alteryx.com>
2019-12-17 17:37:43 -0700

    Adding duration to the actual hasher.

--- encodedhash_test.go
+++ encodedhash_test.go
@@ -4,7 +4,7 @@ import "testing"
 
 func TestHasher(t *testing.T) {
 	expected := "ZEHhWB65gUlzdVwtDQArEyx+KVLzp/aTaRaPlBzYRIFj6vjFdqEb0Q5B8zVKCZ0vKbZPZklJz0Fd7su2A+gf7Q=="
-	result := CalculateHash("angryMonkey")
+	result, _ := CalculateHash("angryMonkey")
 	if expected != result {
 		t.Errorf("CalculateHash() = %q, but expected %q", result, expected)
 	}
